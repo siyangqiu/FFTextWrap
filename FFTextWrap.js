a5eb7a7e459625246752e86ed039834b549396b7
siyangqiu <siyangqiu@gmail.com>
2018-04-28 18:14:36 -0500

    v1.1 - adds view tracking - tries to keep viewport in place after reflow

--- FFTextWrap.js
+++ FFTextWrap.js
@@ -1,7 +1,10 @@
+"use strict";
+
 //Parameters
 var tags = 'p,li,a,h1,h2,h3,h4,h5,h6,tr,td,th';
 var margin = 10;
- 
+var enableTrack = true; //experimental feature to prevent jumping after reflowing text
+var focus = 'top'; //which element to track after zoom (center, top, natural). DEFAULT is top
 
 //Code Start
 
@@ -10,26 +13,63 @@ var init = function () {
     styleNode.type = "text/css";
     styleNode.id = "FFTW";
     document.getElementsByTagName('head')[0].appendChild(styleNode);
-} 
+};
 
-var resize = function(ev) {
-    if(ev.touches.length == 1) {
+var resize = function() {
+    if(enableTrack === true && lastDistance > startDistance) {
+        trackPage();
+        document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + ( window.innerWidth - margin ) + 'px;  }';
+        topElement.scrollIntoView();
+    } else {
         document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + ( window.innerWidth - margin ) + 'px;  }';
     }
-}
+};
 
 //Runtime
 
 init();
 
 document.addEventListener('touchend', function(event) {
-    resize(event);
+    if(event.touches.length == 1) {
+        resize();
+    }
 }, false);
+
+//Optional features
+
+if(enableTrack === true) {
+    var topElement;
+    var startDistance;
+    var lastDistance;
+
+    var trackPage = function() {
+        if(focus == 'center') {
+            topElement = document.elementFromPoint(window.innerWidth/2,window.innerHeight/2);
+        } else if (focus == 'natural'){
+            topElement = document.elementFromPoint(20,20);
+        } else {
+            topElement = document.elementFromPoint(0,0);
+        }
+    };
+
+    document.addEventListener('touchstart', function(event) {
+        if(event.touches.length == 2) {
+            startDistance = Math.hypot(event.touches[0].pageX-event.touches[1].pageX,event.touches[0].pageY-event.touches[1].pageY);
+        }
+    }, false);
+
+    document.addEventListener('touchmove', function(event) {
+        if(event.touches.length == 2) {
+            lastDistance = Math.hypot(event.touches[0].pageX-event.touches[1].pageX,event.touches[0].pageY-event.touches[1].pageY);
+        }
+        
+    }, false);
+}
 
 //Debugging
 
 //For debugging on desktop where there are no Touch Events. 
-//    document.addEventListener('click', function(event) {
-//      console.log("click")
-//      document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + window.innerWidth + margin + 'px;  }';
-//    }, false);
+// document.addEventListener('click', function (event) {
+//     console.log("click");
+//     resize();
+// }, false);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# FFTextWrap
-Personal project for adding textwrap to Fennec Firefox (Firefox on Android). Compared to other text wrap/reflow add-ons on AMO, this reflows the entire page based on touch events - the page will reflow as soon as you finish your pinch to zoom! No more clicking on text elements! Source code at: https://github.com/siyangqiu/FFTextWrap
+# Fennec TextWrap
+Personal project for adding textwrap to Firefox on Android. Compared to other text wrap/reflow add-ons on AMO, this reflows the entire page based on touch events - the page will reflow as soon as you finish your pinch to zoom! No more clicking on text elements! Source code at: https://github.com/siyangqiu/FFTextWrap
