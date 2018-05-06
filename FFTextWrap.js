8111019494f183a404dfd6eda72f57d66a138c37
siyangqiu <siyangqiu@gmail.com>
2018-05-05 23:09:05 -0500

    bugfixes + menu update

--- FFTextWrap.js
+++ FFTextWrap.js
@@ -3,46 +3,53 @@
 //Default values - Not applicable if values are available in storage;
 var tags = 'p,li,ul,a,h1,h2,h3,h4,h5,h6,tr,td,th,label';
 var margin = 10;
-var enableTrack = true; 
+var enableTrack = true;
 var focus = 'center';
+
+//Parameters
 const delay = 1;
 
-//Code Start
+//Functions "Library"
 
-var init = function () {    //Setup: 1. create style tag for later CSS injection. 2. Read values from storage if available. Otherwise
+//Setup: 1. create style tag for later CSS injection. 2. Read values from storage if available.
+var init = function () { 
     var styleNode = document.createElement('style');
     styleNode.type = 'text/css';
     styleNode.id = 'FFTW';
     document.getElementsByTagName('head')[0].appendChild(styleNode);
 
-    var getSettings = browser.storage.local.get(['viewTracking','trackLocation','margin','p','li','ul','a','h1','h2','h3','h4','h5','h6','tr','td','th']);
+    var getSettings = browser.storage.local.get(['viewTracking', 'trackLocation', 'margin', 'p', 'li', 'ul', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'tr', 'td', 'th']);
     getSettings.then(onGetSettings, onError);
+    console.log('gotsettings');
 };
 
-var resize = function() {   //Call this function to reflow page. 
-    setTimeout(function(){  //Give browser chance to recompute window.innerWidth before resizing
-        if(enableTrack === true && lastDistance > startDistance) {  //if enableTrack (whether View Tracking is enabled), we need to 1. grab current view 2.Inject CSS 3. Scroll to 'current view'
+//Call this function to reflow page.
+var resize = function () { 
+    //Give browser chance to recompute window.innerWidth before resizing 
+    setTimeout(function () { 
+        if (enableTrack === true && lastDistance > startDistance) { 
             getCurrentView();
-            document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + ( window.innerWidth - margin ) + 'px;  }';
+            document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + (window.innerWidth - margin) + 'px;  }';
             topElement.scrollIntoView();
-        } else { //otherwise, just inject CSS
-            document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + ( window.innerWidth - margin ) + 'px;  }';
+        } else {
+            document.getElementById('FFTW').innerHTML = tags + '{   max-width: ' + (window.innerWidth - margin) + 'px;  }';
         }
-    },delay);
+    }, delay);
 };
 
-function onGetSettings(res) { //After getting settings, override default parameters
-    if (res != null){
-        enableTrack = res.viewTracking;
-        
-        focus = res.trackElement;
-        
-        if (typeof res.margin === 'number') {
-            margin = res.margin;
+//After getting settings, override default parameters
+var onGetSettings = function (settings) {
+    if (Object.keys(settings).length !== 0) {
+        enableTrack = settings.viewTracking;
+
+        focus = settings.trackElement;
+
+        if (typeof settings.margin === 'number') {
+            margin = settings.margin;
         }
         tags = '';
-        for (var key in res) {
-            if (key != 'margin' && key != 'trackElement' && key != 'viewTracking' && res[key] === true) {
+        for (var key in settings) {
+            if (key != 'margin' && key != 'trackElement' && key != 'viewTracking' && settings[key] === true) {
                 tags += key + ',';
             }
         }
@@ -50,44 +57,50 @@ function onGetSettings(res) { //After getting settings, override default paramet
     }
 }
 
-function onError(error) {
+var onError = function (error) {
     console.log(`Error: ${error}`);
 }
 
-//Runtime
+//"Main"/Runtime start
 
-init(); //call initial setup code
+//call initial setup code
+init(); 
 
-document.addEventListener('touchend', function(event) { //add listener for when the user ends the pinch to zoom (i.e. when the user drops down to 1 finger on the screen). Fire resize event
-    if(event.touches.length == 1) {
+//add listener for when the user ends the pinch to zoom (i.e. when the user drops down to 1 finger on the screen). Fire resize event
+document.addEventListener('touchend', function (event) { 
+    if (event.touches.length == 1) {
         resize();
     }
 }, false);
 
-if(enableTrack === true) {  //if enableTrack (whether View Tracking is enabled), additional code is needed: 1. function for getting the element to track. 2. whether the user zoomed in or out via start distance and end distance
+//if enableTrack (whether View Tracking is enabled), additional code is needed: 1. function for getting the element to track. 2. whether the user zoomed in or out via start distance and end distance
+if (enableTrack === true) { 
     var topElement;
     var startDistance;
     var lastDistance;
 
-    var getCurrentView = function() {   //function for determining the element to track after zoom
-        if(focus == 'center') {
-            topElement = document.elementFromPoint(window.innerWidth/2,window.innerHeight/2);
-        } else if (focus == 'natural'){
-            topElement = document.elementFromPoint(20,20);
+    //function for determining the element to track after zoom
+    var getCurrentView = function () { 
+        if (focus === 'center') {
+            topElement = document.elementFromPoint(window.innerWidth / 2, window.innerHeight / 2);
+        } else if (focus === 'natural') {
+            topElement = document.elementFromPoint(20, 20);
         } else {
-            topElement = document.elementFromPoint(0,0);
+            topElement = document.elementFromPoint(0, 0);
         }
     };
 
-    document.addEventListener('touchstart', function(event) {   //add listener to determine start distance
-        if(event.touches.length == 2) {
-            startDistance = Math.hypot(event.touches[0].pageX-event.touches[1].pageX,event.touches[0].pageY-event.touches[1].pageY);
+    //add listener to determine start distance
+    document.addEventListener('touchstart', function (event) { 
+        if (event.touches.length === 2) {
+            startDistance = Math.hypot(event.touches[0].pageX - event.touches[1].pageX, event.touches[0].pageY - event.touches[1].pageY);
         }
     }, false);
 
-    document.addEventListener('touchmove', function(event) {    //add listener to determine end distance
-        if(event.touches.length == 2) {
-            lastDistance = Math.hypot(event.touches[0].pageX-event.touches[1].pageX,event.touches[0].pageY-event.touches[1].pageY);
+    //add listener to determine end distance
+    document.addEventListener('touchmove', function (event) { 
+        if (event.touches.length === 2) {
+            lastDistance = Math.hypot(event.touches[0].pageX - event.touches[1].pageX, event.touches[0].pageY - event.touches[1].pageY);
         }
     }, false);
 }
@@ -95,7 +108,7 @@ if(enableTrack === true) {  //if enableTrack (whether View Tracking is enabled),
 //Debugging
 
 //For debugging on desktop where there are no Touch Events. 
-// document.addEventListener('click', function (event) {
-//     console.log('click');
-//     resize();
-// }, false);
+document.addEventListener('click', function (event) {
+    console.log('click');
+    resize();
+}, false);
